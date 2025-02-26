13707ca3f3ece3cd185c4622d8b4e9dad3ba1a94
davidetaddeucci <info@hybrid.it>
2025-02-26 11:55:29 +0100

    Aggiunta implementazione del servizio ProgettoUE

---
+++ Microsis.Web.Services/Services/ProgettoUEService.cs
@@ -0,0 +1,111 @@
+using Microsoft.EntityFrameworkCore;
+using Microsis.Names.Models;
+using Microsis.Web.Services.Data;
+
+namespace Microsis.Web.Services.Services
+{
+    /// <summary>
+    /// Implementazione del servizio per la gestione dei progetti UE
+    /// </summary>
+    public class ProgettoUEService : IProgettoUEService
+    {
+        private readonly AppDbContext _context;
+
+        public ProgettoUEService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Ottiene tutti i progetti UE
+        /// </summary>
+        /// <param name="includeHidden">Se true, include anche i progetti nascosti</param>
+        /// <returns>Lista di progetti UE</returns>
+        public async Task<IEnumerable<ProgettoUE>> GetAllAsync(bool includeHidden = false)
+        {
+            IQueryable<ProgettoUE> query = _context.ProgettiUE;
+            
+            if (!includeHidden)
+                query = query.Where(p => p.Visible);
+                
+            return await query.OrderByDescending(p => p.LastUpdate).ToListAsync();
+        }
+
+        /// <summary>
+        /// Ottiene un progetto UE tramite ID
+        /// </summary>
+        /// <param name="id">ID del progetto</param>
+        /// <returns>Progetto UE o null</returns>
+        public async Task<ProgettoUE?> GetByIdAsync(Guid id)
+        {
+            return await _context.ProgettiUE.FindAsync(id);
+        }
+
+        /// <summary>
+        /// Crea un nuovo progetto UE
+        /// </summary>
+        /// <param name="progetto">Dati del progetto</param>
+        /// <param name="author">Autore</param>
+        /// <returns>Progetto creato</returns>
+        public async Task<ProgettoUE> CreateAsync(ProgettoUE progetto, string author)
+        {
+            // Crea un nuovo ID per il progetto se non è già stato impostato
+            if (progetto.ID == Guid.Empty)
+                progetto.ID = Guid.NewGuid();
+
+            // Imposta i campi obbligatori
+            progetto.LastUpdate = DateTime.Now;
+            progetto.Author = author;
+
+            // Salva il progetto
+            await _context.ProgettiUE.AddAsync(progetto);
+            await _context.SaveChangesAsync();
+
+            return progetto;
+        }
+
+        /// <summary>
+        /// Aggiorna un progetto UE esistente
+        /// </summary>
+        /// <param name="progetto">Progetto da aggiornare</param>
+        /// <param name="author">Autore</param>
+        /// <returns>Progetto aggiornato</returns>
+        public async Task<ProgettoUE> UpdateAsync(ProgettoUE progetto, string author)
+        {
+            var progettoToUpdate = await _context.ProgettiUE.FindAsync(progetto.ID)
+                ?? throw new KeyNotFoundException("Progetto non trovato");
+
+            // Aggiorna i campi del progetto
+            progettoToUpdate.Titolo = progetto.Titolo;
+            progettoToUpdate.Abstract = progetto.Abstract;
+            progettoToUpdate.EntiCoinvolti = progetto.EntiCoinvolti;
+            progettoToUpdate.Tab_Name = progetto.Tab_Name;
+            progettoToUpdate.Visible = progetto.Visible;
+            progettoToUpdate.ImagePath = progetto.ImagePath;
+            progettoToUpdate.LastUpdate = DateTime.Now;
+            progettoToUpdate.Author = author;
+
+            // Salva le modifiche
+            _context.ProgettiUE.Update(progettoToUpdate);
+            await _context.SaveChangesAsync();
+
+            return progettoToUpdate;
+        }
+
+        /// <summary>
+        /// Elimina un progetto UE
+        /// </summary>
+        /// <param name="id">ID del progetto</param>
+        /// <returns>True se eliminato con successo</returns>
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var progetto = await _context.ProgettiUE.FindAsync(id);
+            if (progetto == null)
+                return false;
+
+            _context.ProgettiUE.Remove(progetto);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
