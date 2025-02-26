e9228c139bb47833d6ca600597e0dace819f7e6c
davidetaddeucci <info@hybrid.it>
2025-02-26 17:55:30 +0100

    Aggiornato IBannerService in Reserved per usare IBannerAdminService da Shared

--- Microsis.Web.Reserved/Services/IBannerService.cs
+++ Microsis.Web.Reserved/Services/IBannerService.cs
@@ -1,50 +1,14 @@
+// Interfaccia spostata in Microsis.Web.Shared.Services.IBannerAdminService
+// Questo file è mantenuto solo come compatibilità, utilizzare Microsis.Web.Shared.Services.IBannerAdminService
+// File può essere rimosso una volta completata la migrazione
+
 using Microsis.Names.Models;
+using Microsis.Web.Shared.Services;
 
 namespace Microsis.Web.Reserved.Services
 {
-    /// <summary>
-    /// Interfaccia per il servizio client di gestione dei banner nell'area riservata
-    /// </summary>
-    public interface IBannerService
+    // Alias per compatibilità con codice esistente
+    public interface IBannerService : IBannerAdminService
     {
-        /// <summary>
-        /// Ottiene tutti i banner (visibili e nascosti)
-        /// </summary>
-        /// <returns>Lista di banner</returns>
-        Task<IEnumerable<Banner>> GetAllAsync();
-        
-        /// <summary>
-        /// Ottiene tutti i banner visibili ordinati per visualizzazione
-        /// </summary>
-        /// <returns>Lista di banner ordinati</returns>
-        Task<IEnumerable<Banner>> GetOrderedAsync();
-        
-        /// <summary>
-        /// Ottiene un banner tramite ID
-        /// </summary>
-        /// <param name="id">ID del banner</param>
-        /// <returns>Banner o null</returns>
-        Task<Banner?> GetByIdAsync(Guid id);
-        
-        /// <summary>
-        /// Crea un nuovo banner
-        /// </summary>
-        /// <param name="banner">Dati del banner</param>
-        /// <returns>Banner creato</returns>
-        Task<Banner?> CreateAsync(Banner banner);
-        
-        /// <summary>
-        /// Aggiorna un banner esistente
-        /// </summary>
-        /// <param name="banner">Banner da aggiornare</param>
-        /// <returns>Banner aggiornato</returns>
-        Task<Banner?> UpdateAsync(Banner banner);
-        
-        /// <summary>
-        /// Elimina un banner
-        /// </summary>
-        /// <param name="id">ID del banner</param>
-        /// <returns>True se eliminato con successo</returns>
-        Task<bool> DeleteAsync(Guid id);
     }
 }
