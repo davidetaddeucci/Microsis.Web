00df673f543d1128bd7a051b9dd4b928deb2407e
davidetaddeucci <info@hybrid.it>
2025-02-27 09:39:25 +0100

    DB MIgrations

--- Microsis.Web.Services/Migrations/20250226191242_m1.Designer.cs
+++ Microsis.Web.Services/Migrations/20250226191242_m1.Designer.cs
@@ -4,16 +4,19 @@ using Microsis.Web.Services.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Microsis.Web.Services.Data.Migrations
+namespace Microsis.Web.Services.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250226191242_m1")]
+    partial class m1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Microsis.Web.Services/Migrations/20250226191242_m1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microsis.Web.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GalleriaFoto",
+                table: "News");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descrizione",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AuthorId",
+                table: "News",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentoId",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescrizioneId",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImmagineUrl",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "News",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsScheduled",
+                table: "News",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "MainImageId",
+                table: "News",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumeroVisite",
+                table: "News",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledDate",
+                table: "News",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SlugUrl",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TitoloId",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Foto",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "EntityID",
+                table: "Foto",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EntityType",
+                table: "Foto",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Foto",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Banners",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Filename = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Subtitle = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Visible = table.Column<bool>(type: "bit", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banners", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Foto_EntityID",
+                table: "Foto",
+                column: "EntityID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Foto_News_EntityID",
+                table: "Foto",
+                column: "EntityID",
+                principalTable: "News",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Foto_News_EntityID",
+                table: "Foto");
+
+            migrationBuilder.DropTable(
+                name: "Banners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Foto_EntityID",
+                table: "Foto");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "ContentoId",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "DescrizioneId",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "ImmagineUrl",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "IsScheduled",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "MainImageId",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "NumeroVisite",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduledDate",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "SlugUrl",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "TitoloId",
+                table: "News");
+
+            migrationBuilder.DropColumn(
+                name: "EntityID",
+                table: "Foto");
+
+            migrationBuilder.DropColumn(
+                name: "EntityType",
+                table: "Foto");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Foto");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descrizione",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GalleriaFoto",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Foto",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
